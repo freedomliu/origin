7fe577ccaeb6736763777922e6ae5eb2973abf1b
freedomliu <13478666105@162.com>
2019-12-02 12:59:19 +0800

    1

--- src/main/java/com/bedrock/origin/OriginApplication.java
+++ src/main/java/com/bedrock/origin/OriginApplication.java
@@ -1,42 +1,33 @@
 package com.bedrock.origin;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 //扫描 mybatis mapper 包路径
-@MapperScan(basePackages = "com.bedrock.origin.mapper")
+//@MapperScan(basePackages = "com.bedrock.origin.mapper")
 //扫描 所有需要的包  @SpringBootApplication里的@ComponentScan已经默认扫描这个类所在等级及其子目录下的包
-/*@ComponentScan(basePackages= {"com.bedrock.origin"})*/
+//@ComponentScan(basePackages= {"com.bedrock.origin"})
 //开启定时任务
-@EnableScheduling
+//@EnableScheduling
 //开启异步调用方法
-@EnableAsync
+//@EnableAsync
 //开启切面
 @EnableAspectJAutoProxy
-public class OriginApplication extends SpringBootServletInitializer{
+@EnableTransactionManagement
+public class OriginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OriginApplication.class, args);
 	}
 	
-	@Override
+	/*@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
 	{
 		//独立Tomcat部署时运行入口  
 		return builder.sources(OriginApplication.class);
-	}
-	@Bean
-	public RestTemplate restTemplate()
-	{
-		return new RestTemplate();
-	}
+	}*/
+	
 }
--- src/main/java/com/bedrock/origin/controller/WebSocketController.java
+++ src/main/java/com/bedrock/origin/controller/WebSocketController.java
@@ -1,62 +1,38 @@
 package com.bedrock.origin.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bedrock.origin.entity.CarInfo;
+import com.bedrock.origin.mapper.slaver.SlaverCarInfoMapper;
+import com.bedrock.origin.service.RawService;
 
 @Controller
 public class WebSocketController {
+	
+	@Autowired
+	RawService processingService;
 
 	@Autowired
-	private SimpMessagingTemplate messageingTemplate;
-	
+	SlaverCarInfoMapper slaverCarInfoMapper;
+
+	@Autowired
+	private KafkaTemplate kafkaTemplate;
+
 	@GetMapping("websocket.html")
+	@Transactional(rollbackFor = Exception.class, transactionManager = "slaverTransactionManager")
 	public String websocket(ModelMap map)
 	{
+		/*CarInfo carInfo = new CarInfo();
+		carInfo.setId("311111111");
+		slaverCarInfoMapper.insertSelective(carInfo);*/
+		ListenableFuture send = kafkaTemplate.send("carInfo", "test");
 		return "websocket";
 	}
-	
-	@GetMapping("websocket2.html")
-	public String websocket2(ModelMap map,HttpServletRequest request)
-	{
-		request.getSession().setAttribute("username", "admin");
-		return "websocket2";
-	}
-	@GetMapping("websocket3.html")
-	public String websocket3(ModelMap map,HttpServletRequest request)
-	{
-		request.getSession().setAttribute("guest", "guest");
-		return "websocket3";
-	}
-	
-    /**
-     * 当浏览器向服务端发送请求时，通过@MessageMapping映射/welcome这个地址，类似于@RequestMapping
-     * 当服务端有消息时，会对订阅了@SendTo中的路径的浏览器发送消息
-     * @param msg
-     * @return
-     */
-    @MessageMapping("/welcome")
-    @SendTo("/topic/getResponse")
-    public String say(String msg){
-    	/*messageingTemplate.convertAndSend("/topic/getResponse", "msg");*/
-    	System.out.println(msg+"111111111111111111");
-        return msg;
-    }
-    
-    @MessageMapping("/chat")
-    public void handleChat(String msg){
-    	
-    	// user是要发送的对象
-    	///quequ/notificatinons 订阅地址                                                   
-    	messageingTemplate.convertAndSendToUser("admin","/quequ/notifications",msg); 
-    }
     
 }
